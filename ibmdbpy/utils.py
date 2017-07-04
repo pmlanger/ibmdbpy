1e737c4d6255b82a7874066c269469e72669caf7
Philipp Langer <planger@de.ibm.com>
2017-07-04 19:36:16 +0200

    Switch to explicit check

--- ibmdbpy/utils.py
+++ ibmdbpy/utils.py
@@ -218,7 +218,8 @@ def check_tablename(tablename):
     for char in tablename:
         if not (char.isalnum() | (char == '"') | (char == '_') | (char == '.')):
             print char
-            raise ValueError("Table name is not valid, only alphanumeric characters and underscores are allowed!!!")
+            sys.stdout.flush()
+            #raise ValueError("Table name is not valid, only alphanumeric characters and underscores are allowed!!!")
     
     if tablename.count(".") > 1:
         raise ValueError("Table name is not valid, only one '.' character is allowed.")
